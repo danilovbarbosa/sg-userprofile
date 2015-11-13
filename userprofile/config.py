e85fa5e144461ead9b50b79083050e81ebb7881e
Maira B. Carvalho <m.brandao.carvalho@tue.nl>
2015-11-13 14:18:36 +0100

    New requirements for Postgresql

--- userprofile/config.py
+++ userprofile/config.py
@@ -8,15 +8,17 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #Flask stuff
-SQLITE_DB = 'userprofile.db'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, SQLITE_DB)
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#SQLITE_DB = 'userprofile.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, SQLITE_DB)
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = "postgresql://userprofile:userprofile@localhost/userprofile"
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'heytheredonottrytomesswithmemister3'
 
 #Other config
-SQLITE_DB_TEST = 'userprofile_testing.db'
-SQLALCHEMY_DATABASE_URI_TEST = 'sqlite:///' + os.path.join(basedir, SQLITE_DB_TEST)
+#SQLITE_DB_TEST = 'userprofile_testing.db'
+#SQLALCHEMY_DATABASE_URI_TEST = 'sqlite:///' + os.path.join(basedir, SQLITE_DB_TEST)
+SQLALCHEMY_DATABASE_URI_TEST = "postgresql://userprofile:userprofile@localhost/userprofile_test"
 TMPDIR = os.path.join(basedir, 'tmp')
 LOG_FILENAME = "userprofile.log.txt"
 LOG_FILENAME_TEST = "userprofile_testing.log.txt"
