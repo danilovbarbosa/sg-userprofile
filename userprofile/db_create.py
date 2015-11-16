840a45a9dedcc86ae04182a023ca05c3fb77a27f
Maira Carvalho <m.brandao.carvalho@tue.nl>
2015-11-16 14:47:33 +0100

    Removing uneeded migrations scripts and vars

--- userprofile/db_create.py
+++ userprofile/db_create.py
@@ -13,8 +13,8 @@ app = create_app()
 with app.app_context():
     db.create_all()
 
-if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+# if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+#     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+#     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+# else:
+#     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
