4c2643a9aa6c39e104706f6be0c6c5e2c00f9187
Maira Carvalho <m.brandao.carvalho@tue.nl>
2015-12-02 18:18:25 +0100

    Uniformized responses. Added docstrings.

--- userprofile/userprofile_app/errors.py
+++ userprofile/userprofile_app/errors.py
@@ -3,18 +3,27 @@ Creates a few custom exceptions used throughout the application.
 '''
 
 class UsernameExistsException(Exception):
+    '''
+    Username already exists in database.
+    '''
     def __init__(self, value):
         self.value = value
     def __str__(self):
         return repr(self.value)
     
 class SessionidNotFoundException(Exception):
+    '''
+    Session ID not found in database.
+    '''
     def __init__(self, value):
         self.value = value
     def __str__(self):
         return repr(self.value)
     
 class UserNotFoundException(Exception):
+    '''
+    User not found in database.
+    '''
     def __init__(self, value):
         self.value = value
     def __str__(self):
