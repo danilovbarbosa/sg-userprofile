5936a02cc931e57c447f7bce12e7428bb215d6a5
Maira Carvalho <m.brandao.carvalho@tue.nl>
2015-12-04 16:55:08 +0100

    Making it work in Dreamhost

--- userprofile/config-sample.py
+++ userprofile/config-sample.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'heytheredonottrytomesswithmemister3'
 
 #Other config
 SQLALCHEMY_DATABASE_URI_TEST = "mysql://userprofile:userprofile@localhost/userprofile_test"
-TMPDIR = os.path.join(basedir, 'tmp')
+TMPDIR = os.path.join(basedir, '..', 'tmp')
 LOG_FILENAME = "userprofile.log.txt"
 LOG_FILENAME_TEST = "userprofile_testing.log.txt"
 
